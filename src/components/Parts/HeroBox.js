d1f4c3b27121f7efe9a55832e561c5f099691985
Rajkumar <110892356+rkpymb@users.noreply.github.com>
2024-01-10 04:24:48 +0530

    huhuydd

--- src/components/Parts/HeroBox.js
+++ src/components/Parts/HeroBox.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Skeleton from '@mui/material/Skeleton';
 import Mstyles from '../../../Styles/home.module.css'
-// import { MediaFilesUrl, MediaFilesFolder } from '../../../Data/config'
+import { MediaFilesUrl, MediaFilesFolder } from '/Data/config'
 
 
 import {
@@ -28,65 +28,84 @@ function RecentOrders() {
     const blurredImageData = 'data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN88enTfwAJYwPNteQx0wAAAABJRU5ErkJggg==';
 
 
+    const GetSliders = async () => {
+        const dataid = '08c5th4rh86ht57h6g';
+        const sendUM = { dataid }
+        const data = await fetch("/api/V3/List/PostersliderlistWebsite", {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(sendUM)
+        }).then((a) => {
+            return a.json();
+        })
+            .then((parsed) => {
+                console.log(parsed.ReqD.Sliders)
+                setRetdata(parsed.ReqD.Sliders)
+                setIsLoading(false)
+            })
+    }
+    useEffect(() => {
+        GetSliders()
+    }, [router.query])
     return (<div className={Mstyles.Sliderhero}>
-
-        {isLoading &&
+        {isLoading ?
             <div>
+                <h1>Loading..</h1>
+            </div> :
+            <div>
+                <div>
 
-                <Swiper
+                    <Swiper
 
-                    breakpoints={{
-                        768: {
-                            slidesPerView: 1, // Display 2 slides on tablets (768px or more)
-                        },
-                        992: {
-                            slidesPerView: 1, // Display 3 slides on desktop (992px or more)
-                        },
-                    }}
-                    spaceBetween={0}
-                    centeredSlides={false}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: true,
-                    }}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    navigation={true}
-                    modules={[Autoplay, Pagination, Navigation]}
+                        breakpoints={{
+                            768: {
+                                slidesPerView: 1, // Display 2 slides on tablets (768px or more)
+                            },
+                            992: {
+                                slidesPerView: 1, // Display 3 slides on desktop (992px or more)
+                            },
+                        }}
+                        spaceBetween={0}
+                        centeredSlides={false}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: true,
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Autoplay, Pagination, Navigation]}
 
 
-                >
-                    <SwiperSlide className={Mstyles.SwiperImgae}>
+                    >
+                        {Retdata.map((item) => {
+                            return <SwiperSlide className={Mstyles.SwiperImgae} key={item._id} onClick={() => router.push(`${item.url}`)}>
 
-                        <Link href={`/services/virtual-assistant`} >
+                                <div>
+                                    <img src={`${MediaFilesUrl}${MediaFilesFolder}/${item.BigImg}`} alt='img' className={Mstyles.SliderimgDesktop} />
+                                    <img src={`${MediaFilesUrl}${MediaFilesFolder}/${item.SmallImg}`} alt='img' className={Mstyles.SliderimgMobile} />
+                                </div>
 
-                            <div>
-                                <img src='/img/p1.jpg' alt='img' className={Mstyles.SliderimgDesktop} />
-                                <img src='/img/pm1.jpg' alt='img' className={Mstyles.SliderimgMobile} />
-                            </div>
-                        </Link>
+                            </SwiperSlide>
 
-                    </SwiperSlide>
 
-                    <SwiperSlide className={Mstyles.SwiperImgae}>
 
-                        <Link href={`/services/website-development`} >
+                        }
 
-                            <div>
-
-                                <img src='/img/p1.jpg' alt='img' className={Mstyles.SliderimgDesktop} />
-                                <img src='/img/pm1.jpg' alt='img' className={Mstyles.SliderimgMobile} />
-                            </div>
-                        </Link>
-
-                    </SwiperSlide>
+                        )}
 
 
 
 
-                </Swiper>
+
+
+                    </Swiper>
+                </div>
             </div>
+
         }
 
     </div>
